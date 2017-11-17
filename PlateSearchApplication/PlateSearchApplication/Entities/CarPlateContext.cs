30389ded2808fe12973459d39e450d4f00828b22
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-11-17 09:39:22 +0100

    Feat(OrientationExam): Add Context and update Startup.cs

---
+++ PlateSearchApplication/PlateSearchApplication/Entities/CarPlateContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlateSearchApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlateSearchApplication.Entities
+{
+    public class CarPlateContext : DbContext
+    {
+        public CarPlateContext(DbContextOptions<CarPlateContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<CarPlate> CarPlates { get; set; }
+    }
+}
