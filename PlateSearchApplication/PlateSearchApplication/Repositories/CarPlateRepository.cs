313fdae66dc26c20be77d092f2bc6e0355a20513
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-11-17 10:27:37 +0100

    Feat(OrientationExam): Add CarPlateRepository

---
+++ PlateSearchApplication/PlateSearchApplication/Repositories/CarPlateRepository.cs
@@ -0,0 +1,18 @@
+﻿using PlateSearchApplication.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlateSearchApplication.Repositories
+{
+    public class CarPlateRepository
+    {
+        private CarPlateContext CarPlateContext;
+
+        public CarPlateRepository(CarPlateContext carPlateContext)
+        {
+            CarPlateContext = carPlateContext;
+        }
+    }
+}
